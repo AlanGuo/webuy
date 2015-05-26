ad68ab2ef39d03254f2560d4eda2a605c4950421
alan <ggddll123@qq.com>
2015-05-26 09:08:15 +0800

    update 2015/05/26

---
+++ backend/requesthandlers.js
@@ -0,0 +1,31 @@
+var ccap = require('ccap');
+
+var service = {
+	//cgi-bin/account/login
+	login:function(request, response, querystring, postData, config){
+		response.writeHead('200');
+		response.writeHead('Content-Type','application/json');
+		var result = {
+			code:0,
+			data:{},
+			msg:''
+		};
+		response.write(JSON.stringify(result),'utf-8');
+		response.end();
+	},
+
+	//cgi-bin/account/verifycode
+	verifycode:function(request, response, querystring, postData, config){
+		var captcha = ccap(/*width, height, offset*/);
+		var ary = captcha.get();
+		
+		//ary[0] is captcha's text,ary[1] is captcha picture buffer.
+		var text = ary[0];
+		var buffer = ary[1];
+		response.writeHead('200');
+		response.writeHead('Content-Type','image/jpeg');
+		response.end(buffer);
+	}
+};
+
+exports.service = service;
