b25fe35a8e4a9fcf4411871b13d31b715f813994
alan <ggddll123@qq.com>
2015-06-04 09:23:16 +0800

    update 2015/06/04

--- spm_modules/spaseed/1.1.14/lib/stats.js
+++ spm_modules/spaseed/1.1.14/lib/stats.js
@@ -7,20 +7,19 @@ define(function(){
 		 * @param  {string} action 事件操作，必填项，表示访客跟元素交互的行为动作，如"播放"、"收藏"、"翻层"等等。
 		 * @param  {string} label 事件标签，选填项，用于更详细的描述事件，从各个方面都可以，比如具体是哪个视频，哪部小说，翻到了第几层等等
 		 * @param  {string} value 事件值，选填项，整数型，用于填写打分型事件的分值，加载时间型事件的时长，订单型事件的价格等等。
-		 * @param  {string} nodeid div元素id，选填项，填写网页中的div元素id值，用于在“用户视点”功能上重绘元素的事件发生情况。
 		 */
-		 trackEvent:function(category,action,label,value,nodeid){
-		 	_czc.push(["_trackEvent",category,action,label,value,nodeid]);
+		 trackEvent:function(category,action,label,value){
+		 	_hmt.push(["_trackEvent",category,action,label,value]);
 		 },
 
 		 /**
 		 * 虚拟pv
 		 * @method pageView
-		 * @param  {string} contentUrl 为需要统计PV的页面、弹层指定URL地址。
-		 * @param  {string} refererUrl 该受访页面的来源页URL地址。
+		 * 可以通过_hmt.push(['_setAutoPageview', false]);来关闭默认的pv,uv统计
+		 * @param  {string} contentUrl 为需要统计PV的页面、弹层指定URL地址。必须以/开头
 		 */
-		 pageView:function(contentUrl, refererUrl){
-		 	_czc.push([ "_trackPageview",contentUrl,refererUrl]);
+		 pageView:function(contentUrl){
+		 	_hmt.push([ "_trackPageview",contentUrl,refererUrl]);
 		 }
 	};
 
